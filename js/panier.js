11affef9fadae39cc02cd8e8cf60f13b4601be4d
delacaps <ssoumier.pro@gmail.com>
2021-04-18 11:20:27 +0200

    form improved version

--- js/panier.js
+++ js/panier.js
@@ -32,7 +32,7 @@ let displayCart = () => {
     for (let i = 0; i < Object.values(cartItems).length; i++) {
       let product_row = AddElement("tr", productContainer);
       product_row.className = "products";
-      console.log(product_row);
+      
 
       let product_cell_1 = AddElement("td", product_row);
       product_cell_1.innerHTML = Object.values(cartItems)[i].name;
@@ -90,24 +90,29 @@ myform.lastName.addEventListener("change", ()=> {
         } )
 
 
+         //pattern regex email address
+let emailRegExp = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+  //pattern regex postcode
+  let cpRegExp = /[0-9]{5}/;
 
 //*********EMAIL VALIDATION************:
 const validEmail = (inputEmail) => {
  
-  //pattern regex email address
-let emailRegExp = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/gm;
-
+ 
 // small balise catching 
 let small = inputEmail.nextElementSibling;
 
 // Email regex testing 
 if (emailRegExp.test(inputEmail.value)) {
+ 
  small.innerHTML = "Syntaxe Email valide";
  inputEmail.classList.remove('is-invalid');
  inputEmail.classList.add('is-valid')
  small.classList.remove('text-danger');
  small.classList.add('text-success');
 } else{
+  
   small.innerHTML = "Syntaxe Email non valide"
   inputEmail.classList.remove('is-valid');
   inputEmail.classList.add('is-invalid')
@@ -123,9 +128,10 @@ const validText = (inputText) => {
   // small balise catching 
 let small = inputText.nextElementSibling;
 
-  //at least 3 letters
+  //at least 2 letters
 
-  if(inputText.value.length > 2) {
+  if(inputText.value.length > 1) {
+ 
     small.innerHTML = "Entrée valide";
     inputText.classList.remove('is-invalid');
     inputText.classList.add('is-valid')
@@ -146,20 +152,21 @@ let small = inputText.nextElementSibling;
 
 const validCp = (inputCp) => {
  
-  //pattern regex postcode
-let emailRegExp = /[0-9]{5}/g;
+
   
 // small balise catching 
 let small = inputCp.nextElementSibling;
 
 // Post code regex testing 
-if (emailRegExp.test(inputCp.value)) {
+if (cpRegExp.test(inputCp.value)) {
+
  small.innerHTML = "Code postal valide";
  inputCp.classList.remove('is-invalid');
  inputCp.classList.add('is-valid')
  small.classList.remove('text-danger');
  small.classList.add('text-success');
 } else{
+  
   small.innerHTML = "Code Postal non valide"
   inputCp.classList.remove('is-valid');
     inputCp.classList.add('is-invalid')
@@ -172,32 +179,38 @@ if (emailRegExp.test(inputCp.value)) {
 
 
 
-myform.addEventListener("submit", async (event) => {
+myform.addEventListener("submit", (event) => {
    // event is an object
 
-  
+   let products_array = cartItems_To_Array()
+   
 // form validation - Client side
-
-  if (myform.checkValidity() === false) {
-    event.preventDefault();
-   event.stopPropagation();
-  
+// 
+  if (myform.checkValidity() === false || emailRegExp.test(myform.email.value) ===false || cpRegExp.test(myform.cp.value) ===false || firstName.value.length < 2 || lastName.value.length < 2 || city.value.length < 2 || address.value.length < 2) 
+  {
+    
+       event.preventDefault();
+       alert("Merci de renseigner correctement vos coordonées")
+      
   }
   
 
 // if ok, form submission by a POST request
-    else{
     
-let products_array = cartItems_To_Array()
+    
 
-if (products_array.length === 0) {
+
+else if (products_array.length === 0) {
   event.preventDefault();
-  event.stopPropagation();
+
   alert("Votre panier est vide!")
+ 
 }
+
+
 else{
   
-  let postdata = await fetch("http://localhost:3000/api/cameras/order", {
+  fetch("http://localhost:3000/api/cameras/order", {
    
     method : "POST",
     headers : {
@@ -216,13 +229,15 @@ else{
 })
 
 
-// response of the server with the orderId
-let postdata_response = await postdata.json();
 
- window.location.href = "confirmation.html?orderId="+postdata_response.orderId
+ .then((first_response) => first_response.json())
+          .then(
+            (second_response) =>
+              (window.location.href =
+                "confirmation.html?orderId=" + second_response.orderId))
 
 }
- }
+ 
 })
 
 
